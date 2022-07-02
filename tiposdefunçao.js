ec8fdf3c14f7755840d296e8daf6f776156fa27d
Cassiovinicius <cassio.alves7789@gmail.com>
2022-07-02 18:34:52 -0300

    calculadora JavaScript

---
+++ tiposdefunçao.js
@@ -0,0 +1,22 @@
+//tipos de funções 
+
+//declarativa
+/*function funcao(){
+    console.log('tudo certo jovem!');
+}
+
+funcao();*/
+
+//expresão de função
+//com nomeação
+// var funcao = function(){
+//     console.log('sou uma mensagem de expresão');
+// }
+
+// funcao();
+//arrow function
+// var funcao = () =>{
+//     console.log('sou um arrow fuction');
+// }
+
+// funcao();
